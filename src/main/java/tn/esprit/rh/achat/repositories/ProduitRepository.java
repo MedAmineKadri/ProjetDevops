0a70b2cce9795597fed8cae2913bd4f1c9887093
rafed-99 <73440128+rafed-99@users.noreply.github.com>
2022-11-09 23:20:29 +0100

    commit

--- src/main/java/tn/esprit/rh/achat/repositories/ProduitRepository.java
+++ src/main/java/tn/esprit/rh/achat/repositories/ProduitRepository.java
@@ -7,19 +7,4 @@ import tn.esprit.rh.achat.entities.Produit;
 @Repository
 public interface ProduitRepository extends JpaRepository<Produit, Long> {
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	/*@Query("SELECT sum(df.prixTotal) FROM DetailFacture df where df.produit=:produit and df.facture.dateFacture between :startDate"
-			+ " and :endDate and df.facture.active=true")
-	public float getRevenuBrutProduit(@Param("produit") Produit produit, @Param("startDate") Date startDate,
-			@Param("endDate") Date endDate);*/
 }
